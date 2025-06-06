bef8b0556d7cad9bd0d62104f2692b654f176e4b
MarylinCR <maguycr@gmail.com>
2025-06-05 19:05:07 -0500

    Aplicacion que genera un histograma y un grafico de dispercion para los datos

---
+++ app1.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('../vehicles_us.csv') # leer los datos
+
+st.write_stream("Anuncios de ventas de coches")
+
+hist_button = st.button('Crear histograma')
+
+if hist_button:
+
+    st.write('Creacion de histograma para el conjunto de datos de anuncio de ventas de coches')
+    
+    fig = px.histogram(car_data, x="odometer") # crear un histograma
+    fig.show()
+
+    st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Crear grafico de dispercion')
+
+if disp_button:
+
+    st.write('Creacion de grafico de dispercion para el conjunto de datos de anuncio de ventas de coches')
+    
+    fig1 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    fig1.show()
+
+    st.plotly_chart(fig1, use_container_width=True)
+
